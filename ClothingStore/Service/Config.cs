5e02aa95a5c31f8b61746091a2b351847aad1e59
Ilnur <grubbvichy@gmail.com>
2022-12-12 20:12:11 +0300

    Первичные зименения

--- ClothingStore/Service/Config.cs
+++ ClothingStore/Service/Config.cs
@@ -2,6 +2,7 @@
 {
     public class Config
     {
-        public static string ConnectionString = "Data Source=(local); Database=ClothingStore; Persist Security Info=false; User ID='sa'; Password='sa'; MultipleActiveResultSets=True; TrustServerCertificate=True;";
+        public static string ConnectionString = "Server=localhost;Database=MyCompany; User Id=postgres;Password=1111";
+        //public static string ConnectionString = "Data Source=(local); Database=ClothingStore; Persist Security Info=false; User ID='sa'; Password='sa'; MultipleActiveResultSets=True; TrustServerCertificate=True;";
     }
 }
